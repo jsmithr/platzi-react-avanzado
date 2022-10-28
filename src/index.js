f44f4ccc286a09e7af83d6d0fc3776870973c1e4
Johann Smith Reyes Zapata <johann.reyes@pragma.com.co>
2022-10-28 15:48:50 -0500

    Configuration vercel

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+ReactDom.render(<h1>Hola desde elemento</h1>, document.getElementById('app'))
+
+/*
+
+const root = createRoot(document.getElementById('app'));
+
+root.render(
+    <React.StrictMode>
+        div
+    </React.StrictMode>
+);
+
+reportWebVitals();
+
+ */
